8f0e3712f8c3ca9b2fcd99ace4931f7862f5eb7f
mumulkogi <202480230+mumulkogi@users.noreply.github.com>
2025-03-11 21:00:54 +0900

    Update hw1/lib/hw1.ml

--- hw1/lib/hw1.ml
+++ hw1/lib/hw1.ml
@@ -17,21 +17,3 @@ let calc operator x y =
     failwith "Unsupported operation"
 
 (* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
-
-let%test "test_add" = calc '+' 1 2 = 3
-let%test "test_sub" = calc '-' 1 2 = -1
-let%test "test_mul" = calc '*' 3 4 = 12
-
-let%test "test_div" =
-  try let _ = calc '/' 1 0 in false
-  with
-    | Failure msg -> msg = "Divide-by-Zero"
-    | _ -> false
-
-let%test "test_mod" =
-  try let _ = calc '%' 8 5 in false
-  with
-    | Failure msg -> msg = "Unsupported operation"
-    | _ -> false
-
-(* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
