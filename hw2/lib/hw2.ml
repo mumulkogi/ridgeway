74a71868ce55f180c9c4b9db1672d80485ebfcbb
mumulkogi <202480230+mumulkogi@users.noreply.github.com>
2025-03-14 11:40:35 +0900

    Add hw2/lib/hw2.ml

---
+++ hw2/lib/hw2.ml
@@ -0,0 +1,38 @@
+(* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
+
+type id = int
+type tree = Nil | N of id * tree * tree
+
+(* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
+
+let fib (_: int): int = 
+  failwith "Not Implemented!"
+
+let fib_opt (_: int): int = 
+  failwith "Not Implemented!"
+
+let last (_: int list): int = 
+  failwith "Not Implemented!"
+
+let second_last (_: int list): int = 
+  failwith "Not Implemented!"
+
+let len (_: int list): int = 
+  failwith "Not Implemented!"
+
+let rev (_: int list): int list = 
+  failwith "Not Implemented!"
+
+let is_palindrome (_: int list): bool = 
+  failwith "Not Implemented!"
+
+let compress (_: string): (int * char) list = 
+  failwith "Not Implemented!"
+
+let sort (_: int -> int -> bool) (_: int list): int list = 
+  failwith "Not Implemented!"
+
+let traverse (_: tree): id list = 
+  failwith "Not Implemented!"
+
+(* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
---
+++ hw2/lib/hw2_test.ml
@@ -0,0 +1,33 @@
+(* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
+
+let%test _ = Hw2.fib 3 = 2
+let%test _ = Hw2.fib (-3) = -1
+
+let%test _ = Hw2.fib_opt 3 = 2
+let%test _ = Hw2.fib_opt (-3) = -1
+
+let%test _ = try let _ = Hw2.last [] in false with Failure msg -> msg = "The given list is empty"
+let%test _ = Hw2.last [3;2;1] = 1
+
+let%test _ = try let _ = Hw2.second_last [1] in false with Failure msg -> msg = "The given list has a sole element"
+let%test _ = Hw2.second_last [1;2;3] = 2
+
+let%test _ = Hw2.len [] = 0
+let%test _ = Hw2.len [1;2;3;4] = 4
+
+let%test _ = Hw2.rev [] = []
+let%test _ = Hw2.rev [1;2;3;4] = [4;3;2;1]
+
+let%test _ = Hw2.is_palindrome [1;2;1] = true
+let%test _ = Hw2.is_palindrome [1;2;3] = false
+
+let%test _ = Hw2.compress "GGGHH" = [(3, 'G'); (2, 'H')]
+let%test _ = Hw2.compress "abba" = [(1, 'a'); (2, 'b'); (1, 'a')]
+
+let%test _ = Hw2.sort (fun i j -> i < j) [5;4;3;2;1] = [1;2;3;4;5]
+let%test _ = Hw2.sort (fun i j -> i < j) [3;5;3;2;1] = [1;2;3;3;5]
+
+let%test _ = Hw2.traverse (Hw2.Nil) = []
+let%test _ = Hw2.traverse (Hw2.N(1, Hw2.N(2, Hw2.Nil, Hw2.Nil), Hw2.N(3, Hw2.Nil, Hw2.Nil))) = [2;3;1]
+
+(* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
