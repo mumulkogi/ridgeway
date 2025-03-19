8f00761c6ce15a3d73c8d930952720f7c2f11edd
mumulkogi <202480230+mumulkogi@users.noreply.github.com>
2025-03-19 18:48:43 +0900

    Update hw2/lib/hw2.ml

--- hw2/lib/hw2.ml
+++ hw2/lib/hw2.ml
@@ -5,34 +5,113 @@ type tree = Nil | N of id * tree * tree
 
 (* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
 
-let fib (_: int): int = 
-  failwith "Not Implemented!"
+let rec fib (n: int): int = 
+  match n with
+    | 0 -> 0
+    | 1 -> 1
+    | n -> if n < 0 then -1 else fib (n - 1) + fib (n - 2)
 
-let fib_opt (_: int): int = 
-  failwith "Not Implemented!"
+(* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
 
-let last (_: int list): int = 
-  failwith "Not Implemented!"
+let fib_opt (n: int): int = 
+  (* `for (int i = 0; i < n; i++)` *)
+  let rec fib_opt_helper (x: int) (y: int) (i: int): int =
+    if n < 0 then -1
+    else if i < n then fib_opt_helper y (x + y) (i + 1)
+    else x
+  in fib_opt_helper 0 1 0
 
-let second_last (_: int list): int = 
-  failwith "Not Implemented!"
+(* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
 
-let len (_: int list): int = 
-  failwith "Not Implemented!"
+let rec last (values: int list): int = 
+  match values with
+    | [] -> failwith "The given list is empty"
+    | head :: [] -> head
+    | _ :: tail -> last tail 
 
-let rev (_: int list): int list = 
-  failwith "Not Implemented!"
+(* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
 
-let is_palindrome (_: int list): bool = 
-  failwith "Not Implemented!"
+let rec second_last (values: int list): int = 
+  match values with
+    | [] -> failwith "The given list is empty"
+    | _ :: [] -> failwith "The given list has a sole element"
+    | head :: _ :: [] -> head
+    | _ :: tail -> second_last tail
 
-let compress (_: string): (int * char) list = 
-  failwith "Not Implemented!"
+(* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
 
-let sort (_: int -> int -> bool) (_: int list): int list = 
-  failwith "Not Implemented!"
+let rec len (values: int list): int = 
+  match values with
+    | [] -> 0
+    | _ :: tail -> 1 + (len tail)
 
-let traverse (_: tree): id list = 
-  failwith "Not Implemented!"
+(* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
+
+let rec rev (values: int list): int list = 
+  match values with
+    | [] -> []
+    | head :: [] -> [head]
+    | head :: tail -> (rev tail) @ [head]
+
+(* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
+
+let rec is_palindrome (values: int list): bool = 
+  match values with
+    | [] -> true
+    | _ :: [] -> true
+    | head :: second :: [] -> (head = second)
+    | head :: tail -> (
+      (head == last tail) &&
+      match (rev tail) with
+        | [] -> true
+        | _ :: tail -> is_palindrome(tail)
+    )
+
+(* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
+
+let compress_helper (values: char list): (int * char) list = 
+  match values with
+    | [] -> []
+    | head :: [] -> [(1, head)]
+    | _ :: _ -> failwith "Not Implemented!"
+
+let compress (str: string): (int * char) list = 
+  let values: char list = List.of_seq (String.to_seq str) in
+  compress_helper values
+
+(* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
+
+let split (values: int list): (int list * int list) = 
+  let rec split_helper (values: int list) (left: int list) (right: int list): 
+    (int list * int list) =
+    match values with 
+      | [] -> (left, right)
+      | head :: [] -> (head :: left, right)
+      | head :: second :: tail ->
+        split_helper tail (head :: left) (second :: right)
+  in split_helper values [] []
+
+let rec merge (f: int -> int -> bool) (left: int list) (right: int list): 
+  int list =
+    match (left, right) with
+    | ([], tail) -> tail
+    | (head, []) -> head
+    | (head1 :: tail1), (head2 :: tail2) ->
+      if (f head1 head2) then head1 :: merge f tail1 (head2 :: tail2)
+      else head2 :: merge f (head1 :: tail1) tail2
+
+let rec sort (f: int -> int -> bool) (values: int list): int list =
+  match values with
+    | [] -> []
+    | head :: [] -> [head]
+    | _ -> let (left, right) = split values 
+           in merge f (sort f left) (sort f right)
+
+(* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
+
+let rec traverse (root: tree): id list = 
+  match root with
+    | Nil -> []
+    | N(value, left, right) -> (traverse left) @ (traverse right) @ [value]
 
 (* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
--- hw2/lib/hw2_test.ml
+++ hw2/lib/hw2_test.ml
@@ -1,9 +1,9 @@
 (* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
 
-let%test _ = Hw2.fib 3 = 2
+let%test _ = Hw2.fib 4 = 3
 let%test _ = Hw2.fib (-3) = -1
 
-let%test _ = Hw2.fib_opt 3 = 2
+let%test _ = Hw2.fib_opt 40 = 102334155
 let%test _ = Hw2.fib_opt (-3) = -1
 
 let%test _ = try let _ = Hw2.last [] in false with Failure msg -> msg = "The given list is empty"
@@ -20,10 +20,20 @@ let%test _ = Hw2.rev [1;2;3;4] = [4;3;2;1]
 
 let%test _ = Hw2.is_palindrome [1;2;1] = true
 let%test _ = Hw2.is_palindrome [1;2;3] = false
+let%test _ = Hw2.is_palindrome [1;2;2;1] = true
+let%test _ = Hw2.is_palindrome [1;2;3;1] = false
+let%test _ = Hw2.is_palindrome [1;2;3;2;1] = true
+let%test _ = Hw2.is_palindrome [1;2;3;4;1] = false
 
+let%test _ = Hw2.compress "Z" = [(1, 'Z')]
+let%test _ = Hw2.compress "AA" = [(2, 'A')]
+let%test _ = Hw2.compress "AB" = [(1, 'A'); (1, 'B')]
 let%test _ = Hw2.compress "GGGHH" = [(3, 'G'); (2, 'H')]
 let%test _ = Hw2.compress "abba" = [(1, 'a'); (2, 'b'); (1, 'a')]
 
+let%test _ = Hw2.sort (fun i j -> i < j) [] = []
+let%test _ = Hw2.sort (fun i j -> i > j) [1] = [1]
+let%test _ = Hw2.sort (fun i j -> i > j) [1;2;3;4] = [4;3;2;1]
 let%test _ = Hw2.sort (fun i j -> i < j) [5;4;3;2;1] = [1;2;3;4;5]
 let%test _ = Hw2.sort (fun i j -> i < j) [3;5;3;2;1] = [1;2;3;3;5]
 
