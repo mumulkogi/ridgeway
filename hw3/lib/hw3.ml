d0d21a53b6e51c424205290866a78b37815515af
mumulkogi <202480230+mumulkogi@users.noreply.github.com>
2025-03-21 12:52:25 +0900

    Update hw3/lib/hw3.ml

--- hw3/lib/hw3.ml
+++ hw3/lib/hw3.ml
@@ -1,8 +1,23 @@
 (* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
 
+type state =
+  | QE
+  | Q0
+  | Q1
+  | Q2
+  | Q3
+  | Q4
+  | Q5
+  | Q6
+  | Q7
+  | Q8
+  | Q9
+  | Q10
+  | Q11
+
 type token =
-  | IDENT of string   (* `[a-z][a-zA-Z_'] *)
-  | NUMBER of string  (* `[1-9][0-9]` *)
+  | IDENT of string   (* `[a-z][a-zA-Z_']*` *)
+  | NUMBER of string  (* `[1-9][0-9]*` *)
   | KW_LET            (* `let` *)
   | KW_IN             (* `in` *)
   | OP_EQ             (* `=` *)
@@ -22,8 +37,54 @@ type parse_stack_elem =
 
 (* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
 
-let lex (_: string): token =
-  failwith "Not Implemented!"
+let transition (s: state) (c: char): state =
+  match (s, c) with
+    (* `KW_LET`, `KW_IN`, `[a-z][a-zA-Z_']*` *)
+
+    | (Q0, 'a'..'z') -> if c = 'l' then Q1 else if c = 'i' then Q7 else Q4
+    | (Q1, 'a'..'z') -> if c = 'e' then Q2 else Q4
+    | (Q2, 'a'..'z') -> if c = 't' then Q3 else Q4
+    | (Q3, 'a'..'z') -> Q4
+    | (Q7, 'a'..'z') -> if c = 'n' then Q8 else Q4
+    | (_, 'A'..'Z') | (_, '_') | (_, '\'') -> if s = Q0 then QE else Q4
+
+(* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
+
+    (* `[1-9][0-9]*` *)
+
+    | (Q0, '1'..'9') -> Q6
+    | (Q6, '0'..'9') -> Q6
+
+(* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
+
+    (* `OP_EQ`, `OP_PLUS`, `OP_MINUS` *)
+
+    | (Q0, '=') -> Q9
+    | (Q0, '+') -> Q10
+    | (Q0, '-') -> Q11
+
+(* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
+
+    | (_, _) -> QE
+
+(* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
+
+let lex (str: string): token =
+  let rec lex_helper (s: state) (cl: char list): token =
+    match cl with
+      | [] -> (
+          match s with
+            | Q4 -> IDENT str
+            | Q6 -> NUMBER str
+            | Q3 -> KW_LET
+            | Q8 -> KW_IN
+            | Q9 -> OP_EQ
+            | Q10 -> OP_PLUS
+            | Q11 -> OP_MINUS
+            | _ -> failwith ("Lexing error: " ^ str)
+        )
+      | head :: tail -> lex_helper (transition s head) tail
+  in lex_helper Q0 (String.to_seq str |> List.of_seq)
 
 (* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
 
--- hw3/lib/hw3_test.ml
+++ hw3/lib/hw3_test.ml
@@ -1,13 +1,39 @@
 (* lex *)
 let%test _ = Hw3.lex "1320" = NUMBER "1320"
 
+let%test _ = Hw3.lex "let" = KW_LET
+let%test _ = Hw3.lex "in" = KW_IN
+
+let%test _ = Hw3.lex "lets" = IDENT "lets"
+let%test _ = Hw3.lex "lETS" = IDENT "lETS"
+
+let%test _ = Hw3.lex "=" = OP_EQ
+let%test _ = Hw3.lex "+" = OP_PLUS
+let%test _ = Hw3.lex "-" = OP_MINUS
+
+let%test _ = 
+  try
+    let _ = Hw3.lex " " in
+    false
+  with
+  | Failure msg -> msg = "Lexing error:  "
+  | _ -> false
+
+let%test _ = 
+  try
+    let _ = Hw3.lex "0" in
+    false
+  with
+  | Failure msg -> msg = "Lexing error: 0"
+  | _ -> false
+
 let%test _ = 
   try
     let _ = Hw3.lex "Answer" in
     false
   with
   | Failure msg -> msg = "Lexing error: Answer"
-  |_ -> false
+  | _ -> false
 
 (* parse *)
 let%test _ = Hw3.parse "let x = 1 in 4 + x" = LetIn ("x", Num "1", Plus (Num "4", Id "x"))
