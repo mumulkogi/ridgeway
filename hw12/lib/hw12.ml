0af87db77c347e3f68653ae41b100dd0af72ee0a
mumulkogi <202480230+mumulkogi@users.noreply.github.com>
2025-05-24 16:01:07 +0900

    Update hw12/lib/hw12.ml

--- hw12/lib/hw12.ml
+++ hw12/lib/hw12.ml
@@ -1,6 +1,7 @@
 (* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
 
-let rec interp_expr (e: Ast.expr) (z: Env.t) (m: Mem.t): Value.t =
+let rec interp_expr (e: Ast.expr) (zm: (Env.t * Mem.t)): Value.t =
+  let (z, m) = zm in
   match e with
     | Ast.Num n -> NumV n
     | Ast.Ref x -> AddrV (Env.find x z)
@@ -10,8 +11,8 @@ let rec interp_expr (e: Ast.expr) (z: Env.t) (m: Mem.t): Value.t =
     | Ast.Sub (e1, e2)
     | Ast.Lt (e1, e2)
     | Ast.Gt (e1, e2) ->
-      let v1: Value.t = interp_expr e1 z m in
-      let v2: Value.t = interp_expr e2 z m in
+      let v1: Value.t = interp_expr e1 zm in
+      let v2: Value.t = interp_expr e2 zm in
       (
         match (v1, v2) with
           | (NumV n1, NumV n2) -> (
@@ -28,8 +29,8 @@ let rec interp_expr (e: Ast.expr) (z: Env.t) (m: Mem.t): Value.t =
           )
       )
     | Ast.Eq (e1, e2) ->
-      let v1: Value.t = interp_expr e1 z m in
-      let v2: Value.t = interp_expr e2 z m in
+      let v1: Value.t = interp_expr e1 zm in
+      let v2: Value.t = interp_expr e2 zm in
       (
         match (v1, v2) with
           | (NumV n1, NumV n2) -> BoolV (n1 == n2)
@@ -38,12 +39,12 @@ let rec interp_expr (e: Ast.expr) (z: Env.t) (m: Mem.t): Value.t =
           | _ -> BoolV false
       )
     | Ast.And (e1, e2) ->
-      let v1: Value.t = interp_expr e1 z m in
+      let v1: Value.t = interp_expr e1 zm in
       (
         match v1 with
           | BoolV b1 ->
             if b1 then (
-              let v2: Value.t = interp_expr e2 z m in
+              let v2: Value.t = interp_expr e2 zm in
               (
                 match v2 with
                   | BoolV b2 -> BoolV (b1 == b2)
@@ -59,12 +60,12 @@ let rec interp_expr (e: Ast.expr) (z: Env.t) (m: Mem.t): Value.t =
           )
       )
     | Ast.Or (e1, e2) ->
-      let v1: Value.t = interp_expr e1 z m in
+      let v1: Value.t = interp_expr e1 zm in
       (
         match v1 with
           | BoolV b1 ->
             if not b1 then (
-              let v2: Value.t = interp_expr e2 z m in
+              let v2: Value.t = interp_expr e2 zm in
               (
                 match v2 with
                   | BoolV _ -> v2
@@ -81,5 +82,55 @@ let rec interp_expr (e: Ast.expr) (z: Env.t) (m: Mem.t): Value.t =
       )
 
 (* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
+
+let rec interp_stmt (s: Ast.stmt) (u: Fstore.t) (zm: (Env.t * Mem.t)):
+  (Env.t * Mem.t) = 
+  let (z, m) = zm in
+  match s with
+    | Ast.DefStmt (x, e) ->
+      let v: Value.t = interp_expr e zm in
+      let a: Env.addr = AddrManager.new_addr () in
+      ((Env.add x a z), (Mem.add a v m))
+    | Ast.IfStmt (e, sl1, sl2) ->
+      let v: Value.t = interp_expr e zm in
+      (
+        match v with
+          | BoolV b -> 
+            let (_, m1) = interp_stmts (if b then sl1 else sl2) u zm in
+            (z, m1)
+          | _ -> failwith (
+            Format.asprintf "Not a bool: %a"
+            Ast.pp_expr e
+          )
+      )
+    | Ast.LoopStmt (e, sl) -> 
+      let v: Value.t = interp_expr e zm in
+      (
+        match v with
+          | BoolV b -> (
+              if b then (
+                let (_, m1) = interp_stmts sl u zm in
+                let (_, m2) = interp_stmt s u (z, m1) in
+                (z, m2)
+              ) else zm
+            )
+          | _ -> failwith (
+            Format.asprintf "Not a bool: %a"
+            Ast.pp_expr e
+          )
+      )
+    | _ -> failwith "Not Implemented!"
+
+and interp_stmts (sl: Ast.stmt list) (u: Fstore.t) (zm: (Env.t * Mem.t)):
+  (Env.t * Mem.t) =
+  let acc: (Env.t * Mem.t) = zm in
+  List.fold_left (fun acc s -> interp_stmt s u acc) acc sl
+
+(* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
+
+let interp_prog (p: Ast.prog): (Env.t * Mem.t) =
+  let _: unit = AddrManager.init () in
+  let (Ast.Program (_, _)) = p in
+  failwith "Not Implemented!"
 
 (* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
--- hw12/lib/hw12_test.ml
+++ hw12/lib/hw12_test.ml
@@ -5,13 +5,13 @@ module HwX = Hw12
 (* [`HwX.interp_expr`] ::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
 
 let%test "HwX_interp_expr_add_00" = HwX.interp_expr 
-  (Ast.Add (Ast.Num 1, Ast.Num 2)) [] []
+  (Ast.Add (Ast.Num 1, Ast.Num 2)) ([], [])
   = Value.NumV 3
 
 let%test "HwX_interp_expr_add_01" = 
   try
     let _ = HwX.interp_expr 
-      (Ast.Add (Ast.Num 1, Ast.Bool true)) [] [] 
+      (Ast.Add (Ast.Num 1, Ast.Bool true)) ([], [])
     in false
   with
     | Failure msg -> msg = "Not a number: 1 + true"
@@ -19,13 +19,13 @@ let%test "HwX_interp_expr_add_01" =
 (* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
 
 let%test "HwX_interp_expr_sub_00" = HwX.interp_expr 
-  (Ast.Sub (Ast.Num 1, Ast.Num 2)) [] []
+  (Ast.Sub (Ast.Num 1, Ast.Num 2)) ([], [])
   = Value.NumV (-1)
 
 let%test "HwX_interp_expr_sub_01" = 
   try
     let _ = HwX.interp_expr 
-      (Ast.Sub (Ast.Num 1, Ast.Bool true)) [] []
+      (Ast.Sub (Ast.Num 1, Ast.Bool true)) ([], [])
     in false
   with
     | Failure msg -> msg = "Not a number: 1 - true"
@@ -33,13 +33,13 @@ let%test "HwX_interp_expr_sub_01" =
 (* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
 
 let%test "HwX_interp_expr_lt_00" = HwX.interp_expr 
-  (Ast.Lt (Ast.Num 1, Ast.Num 2)) [] []
+  (Ast.Lt (Ast.Num 1, Ast.Num 2)) ([], [])
   = Value.BoolV true
 
-let%test "HwX_interp_prog_lt_01" = 
+let%test "HwX_interp_expr_lt_01" = 
   try
     let _ = HwX.interp_expr 
-      (Ast.Lt (Ast.Num 1, Ast.Bool true)) [] []
+      (Ast.Lt (Ast.Num 1, Ast.Bool true)) ([], [])
     in false
   with
     | Failure msg -> msg = "Not a number: 1 < true"
@@ -47,13 +47,13 @@ let%test "HwX_interp_prog_lt_01" =
 (* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
 
 let%test "HwX_interp_expr_gt_00" = HwX.interp_expr 
-  (Ast.Gt (Ast.Num 1, Ast.Num 2)) [] []
+  (Ast.Gt (Ast.Num 1, Ast.Num 2)) ([], [])
   = Value.BoolV false
 
-let%test "HwX_interp_prog_gt_01" = 
+let%test "HwX_interp_expr_gt_01" = 
   try
     let _ = HwX.interp_expr 
-      (Ast.Gt (Ast.Num 1, Ast.Bool true)) [] []
+      (Ast.Gt (Ast.Num 1, Ast.Bool true)) ([], [])
     in false
   with
     | Failure msg -> msg = "Not a number: 1 > true"
@@ -61,47 +61,47 @@ let%test "HwX_interp_prog_gt_01" =
 (* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
 
 let%test "HwX_interp_expr_eq_00" = HwX.interp_expr 
-  (Ast.Eq (Ast.Num 1, Ast.Num 1)) [] []
+  (Ast.Eq (Ast.Num 1, Ast.Num 1)) ([], [])
   = Value.BoolV true
 
 let%test "HwX_interp_expr_eq_01" = HwX.interp_expr 
-  (Ast.Eq (Ast.Num 1, Ast.Num 2)) [] [] 
+  (Ast.Eq (Ast.Num 1, Ast.Num 2)) ([], []) 
   = Value.BoolV false
 
 let%test "HwX_interp_expr_eq_02" = HwX.interp_expr 
-  (Ast.Eq (Ast.Num 1, Ast.Bool true)) [] []
+  (Ast.Eq (Ast.Num 1, Ast.Bool true)) ([], [])
   = Value.BoolV false
 
 let%test "HwX_interp_expr_eq_03" = HwX.interp_expr 
-  (Ast.Eq (Ast.Bool false, Ast.Bool true)) [] []
+  (Ast.Eq (Ast.Bool false, Ast.Bool true)) ([], [])
   = Value.BoolV false
 
 let%test "HwX_interp_expr_eq_04" = HwX.interp_expr 
-  (Ast.Eq (Ast.Ref "x", Ast.Ref "y")) [("x", 0xDEADBEEF); ("y", 0xDEADC0DE)] []
+  (Ast.Eq (Ast.Ref "x", Ast.Ref "y")) ([("x", 0xDEAD); ("y", 0xC0DE)], [])
   = Value.BoolV false
 
 (* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
 
 let%test "HwX_interp_expr_and_00" = HwX.interp_expr 
-  (Ast.And (Ast.Bool true, Ast.Bool true)) [] []
+  (Ast.And (Ast.Bool true, Ast.Bool true)) ([], [])
   = Value.BoolV true
 
 let%test "HwX_interp_expr_and_01" = HwX.interp_expr 
-  (Ast.And (Ast.Bool true, Ast.Bool false)) [] []
+  (Ast.And (Ast.Bool true, Ast.Bool false)) ([], [])
   = Value.BoolV false
 
 let%test "HwX_interp_expr_and_02" = HwX.interp_expr 
-  (Ast.And (Ast.Bool false, Ast.Bool true)) [] []
+  (Ast.And (Ast.Bool false, Ast.Bool true)) ([], [])
   = Value.BoolV false
 
 let%test "HwX_interp_expr_and_03" = HwX.interp_expr 
-  (Ast.And (Ast.Bool false, Ast.Bool false)) [] []
+  (Ast.And (Ast.Bool false, Ast.Bool false)) ([], [])
   = Value.BoolV false
 
 let%test "HwX_interp_expr_and_04" = 
   try
     let _ = HwX.interp_expr 
-      (Ast.And (Ast.Num 1, Ast.Bool true)) [] []
+      (Ast.And (Ast.Num 1, Ast.Bool true)) ([], [])
     in false
   with
     | Failure msg -> msg = "Not a bool: 1 && true"
@@ -109,7 +109,7 @@ let%test "HwX_interp_expr_and_04" =
 let%test "HwX_interp_expr_and_05" = 
   try
     let _ = HwX.interp_expr 
-      (Ast.And (Ast.Bool true, Ast.Num 1)) [] []
+      (Ast.And (Ast.Bool true, Ast.Num 1)) ([], [])
     in false
   with
     | Failure msg -> msg = "Not a bool: true && 1"
@@ -117,25 +117,25 @@ let%test "HwX_interp_expr_and_05" =
 (* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
 
 let%test "HwX_interp_expr_or_00" = HwX.interp_expr 
-  (Ast.Or (Ast.Bool true, Ast.Bool true)) [] []
+  (Ast.Or (Ast.Bool true, Ast.Bool true)) ([], [])
   = Value.BoolV true
 
 let%test "HwX_interp_expr_or_01" = HwX.interp_expr 
-  (Ast.Or (Ast.Bool true, Ast.Bool false)) [] []
+  (Ast.Or (Ast.Bool true, Ast.Bool false)) ([], [])
   = Value.BoolV true
 
 let%test "HwX_interp_expr_or_02" = HwX.interp_expr 
-  (Ast.Or (Ast.Bool false, Ast.Bool true)) [] [] 
+  (Ast.Or (Ast.Bool false, Ast.Bool true)) ([], [])
   = Value.BoolV true
 
 let%test "HwX_interp_expr_or_03" = HwX.interp_expr 
-  (Ast.Or (Ast.Bool false, Ast.Bool false)) [] [] 
+  (Ast.Or (Ast.Bool false, Ast.Bool false)) ([], [])
   = Value.BoolV false
 
 let%test "HwX_interp_expr_or_04" = 
   try
     let _ = HwX.interp_expr 
-      (Ast.Or (Ast.Num 1, Ast.Bool true)) [] []
+      (Ast.Or (Ast.Num 1, Ast.Bool true)) ([], [])
     in false
   with
     | Failure msg -> msg = "Not a bool: 1 || true"
@@ -143,9 +143,15 @@ let%test "HwX_interp_expr_or_04" =
 let%test "HwX_interp_expr_or_05" = 
   try
     let _ = HwX.interp_expr 
-      (Ast.Or (Ast.Bool false, Ast.Num 1)) [] [] 
+      (Ast.Or (Ast.Bool false, Ast.Num 1)) ([], [])
     in false
   with
     | Failure msg -> msg = "Not a bool: false || 1"
+
+(* [`HwX.interp_stmt`] ::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
+
+(* [`HwX.interp_fundef`] ::::::::::::::::::::::::::::::::::::::::::::::::::: *)
+
+(* [`HwX.interp_prog`] ::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
 
 (* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
