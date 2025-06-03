4c2ec1b66bd5d7a83e6777f8023cad8a332419f5
mumulkogi <202480230+mumulkogi@users.noreply.github.com>
2025-06-03 21:04:15 +0900

    Update hw13/lib/hw13_tc.ml

--- hw13/lib/hw13_tc.ml
+++ hw13/lib/hw13_tc.ml
@@ -1,14 +1,40 @@
 (* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
 
-let tc_expr (e: Ast.expr): (GlobalTEnv.t * LocalTEnv.t) =
-  match e with
-    | _ -> failwith "Not Implemented!" 
+module F = Format
 
 (* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
 
-let tc_stmt (s: Ast.stmt): (GlobalTEnv.t * LocalTEnv.t) =
+let tc_expr (e: Ast.expr) (h: LocalTEnv.t): Ast.typ =
+  match e with
+    | Ast.Num _ -> Ast.TInt
+
+    | Ast.Ref x ->
+      let y: Ast.typ = (
+        try
+          LocalTEnv.find x h
+        with
+          | Failure _ -> failwith (
+              F.asprintf "[Ill-typed] %a" Ast.pp_expr e
+            )
+      ) in (Ast.TPtr y)
+
+    | _ -> failwith "Not Implemented!"
+
+(* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
+
+let tc_stmt (s: Ast.stmt) (gh: (GlobalTEnv.t * LocalTEnv.t)): 
+  (GlobalTEnv.t * LocalTEnv.t) =
+  let (_, _) = gh in
   match s with
-    | _ -> failwith "Not Implemented!" 
+    | _ -> failwith "Not Implemented!"
+
+(* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
+
+let tc_fundef (d: Ast.def) (gh: (GlobalTEnv.t * LocalTEnv.t)): 
+  (GlobalTEnv.t * LocalTEnv.t) =
+  let (_, _) = gh in
+  match d with
+    | _ -> failwith "Not Implemented!"
 
 (* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: *)
 
